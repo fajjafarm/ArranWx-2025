5a02dfc00d74dfee958b6e3869176cbff0962624
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-11 16:51:13 +0000

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace App\Http\Controllers;
 
 use App\Services\WeatherService;
@@ -14,8 +14,50 @@ class WeatherController extends Controller
     {
         $this->weatherService = $weatherService;
     }
+    public function index()
+    {
+        $locations = Location::all();
+        $weatherData = [];
 
-    // ... index method unchanged ...
+        foreach ($locations as $location) {
+            // Cache weather data for 1 hour (3600 seconds)
+            $weatherCacheKey = "weather_{$location->latitude}_{$location->longitude}";
+            $weather = Cache::remember($weatherCacheKey, 3600, function () use ($location) {
+                return $this->weatherService->getWeather($location->latitude, $location->longitude);
+            });
+            $weatherDetails = $weather['properties']['timeseries'][0]['data']['instant']['details'];
+
+            $marine = null;
+            if ($location->type === 'Marine') {
+                // Cache marine data for 1 hour (3600 seconds)
+                $marineCacheKey = "marine_{$location->latitude}_{$location->longitude}";
+                $marineResponse = Cache::remember($marineCacheKey, 3600, function () use ($location) {
+                    return $this->weatherService->getMarineForecast($location->latitude, $location->longitude);
+                });
+                if ($marineResponse) {
+                    $marine = [
+                        'wave_height' => $marineResponse['hourly']['wave_height'][0] ?? null,
+                        'wave_direction' => $marineResponse['hourly']['wave_direction'][0] ?? null,
+                        'wave_period' => $marineResponse['hourly']['wave_period'][0] ?? null,
+                        'wind_wave_height' => $marineResponse['hourly']['wind_wave_height'][0] ?? null,
+                        'swell_wave_height' => $marineResponse['hourly']['swell_wave_height'][0] ?? null,
+                        'swell_wave_direction' => $marineResponse['hourly']['swell_wave_direction'][0] ?? null,
+                        'swell_wave_period' => $marineResponse['hourly']['swell_wave_period'][0] ?? null,
+                        'water_temperature' => $marineResponse['hourly']['water_temperature'][0] ?? null,
+                    ];
+                }
+            }
+
+            $weatherData[$location->name] = [
+                'weather' => $weatherDetails,
+                'marine' => $marine,
+                'type' => $location->type,
+                'altitude' => $location->altitude ?? 0,
+            ];
+        }
+
+        return view('dashboard', compact('weatherData', 'locations'));
+    }
 
     public function show($name)
     {
