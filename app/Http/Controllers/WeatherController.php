6b05f7e40dcdcbbcdeafb5835f0bdb267dd9d191
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-03-13 19:53:09 +0000

    Update WeatherController.php

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -196,7 +196,7 @@ class WeatherController extends Controller
                     $details = $entry['data']['instant']['details'];
                     $next1Hour = $entry['data']['next_1_hours'] ?? ['summary' => ['symbol_code' => 'N/A'], 'details' => ['precipitation_amount' => 0]];
                     if ($next1Hour['summary']['symbol_code'] == 'N/A'){
-                    $next1our = $entry['data']['next_6_hours'] ?? ['summary' => ['symbol_code' => 'N/A'], 'details' => ['precipitation_amount' => 0]];
+                    $next1Hour = $entry['data']['next_6_hours'] ?? ['summary' => ['symbol_code' => 'N/A'], 'details' => ['precipitation_amount' => 0]];
                     }
                     // Calculate Gust
                     $windSpeed = $details['wind_speed'] ?? 0;
