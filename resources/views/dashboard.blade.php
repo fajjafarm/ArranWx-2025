d243ae13a075dc0bac74c4605c889312b41e7e18
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-04-17 19:23:53 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,7 +23,7 @@
         }
         .card-body {
             color: #fff;
-            text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+           
         }
         .status-normal { background-color: #28a745; }
         .status-yellow { background-color: #ffc107; }
