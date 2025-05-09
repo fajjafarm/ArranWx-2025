385c0d4cb4fc874e25fdccd3d67599a07e11133d
fajjafarm <67113113+fajjafarm@users.noreply.github.com>
2025-05-09 19:56:14 +0100

    1

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -3,7 +3,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Carbon\Carbon;
+
 
 class ResourcesController extends Controller
 {
--- app/Http/Controllers/RoutingController.php
+++ app/Http/Controllers/RoutingController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Carbon\Carbon;
+
 
 class RoutingController extends Controller
 {
